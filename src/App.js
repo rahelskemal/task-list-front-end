c648e9d5fa78a124026578cb228cb38aef31e803
reneesu99 <reneesu22@gmail.com>
2022-12-15 11:58:36 -0800

    added event handling kinda

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-
 const TASKS = [
   {
     id: 1,
@@ -15,14 +14,31 @@ const TASKS = [
   },
 ];
 
+
 const App = () => {
+  const [tasks, setTask] = useState(TASKS)
+
+  const updateTask = (updatedTask) => {
+    console.log("updateTask", updatedTask);
+    let newTasks 
+    newTasks = tasks.map(task =>{
+      if (task.id === updatedTask.id){ 
+        task = updatedTask;
+        console.log(task.isComplete)
+      return task;
+      }
+    console.log(newTasks)
+    // setTask(newTasks);
+    }
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={tasks} updateTask = {updateTask} />}</div>
       </main>
     </div>
   );
